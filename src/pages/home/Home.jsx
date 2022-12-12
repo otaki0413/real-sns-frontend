d61ddc8af96c0eaf84b7d236230cb86fe8f7ae11
otaki0413 <takida0413@gmail.com>
2022-12-13 00:30:46 +0900

    ✨ Sidebar, Timeline, Rightbarコンポーネントの雛形を作成

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
+import { Rightbar } from "../../components/rightbar/Rightbar";
+import { Sidebar } from "../../components/sidebar/Sidebar";
+import { Timeline } from "../../components/timeline/Timeline";
 import { Topbar } from "../../components/topbar/Topbar";
 
 export const Home = () => {
   return (
     <div>
       <Topbar />
-      {/* sideber */}
-      {/* sideber */}
-      {/* sideber */}
+      <Sidebar />
+      <Timeline />
+      <Rightbar />
     </div>
   );
 };
