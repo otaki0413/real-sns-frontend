a18c50ba469e9ed5eae54406a4e6b058d8ffb419
otaki0413 <takida0413@gmail.com>
2022-12-14 00:37:56 +0900

    💄 Shareコンポーネントのスタイリング

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -23,23 +23,35 @@ export const Share = () => {
         <div className="shareBottom">
           <div className="shareButtons">
             <div className="shareButton">
-              <Image className="shareIcon" />
+              <Image
+                className="shareIcon"
+                htmlColor="blue"
+              />
               <span className="shareIconText">画像</span>
             </div>
             <div className="shareButton">
-              <Gif className="shareIcon" />
+              <Gif
+                className="shareIcon"
+                htmlColor="hotpink"
+              />
               <span className="shareIconText">GIF</span>
             </div>
             <div className="shareButton">
-              <Face className="shareIcon" />
+              <Face
+                className="shareIcon"
+                htmlColor="green"
+              />
               <span className="shareIconText">気持ち</span>
             </div>
             <div className="shareButton">
-              <Analytics className="shareIcon" />
+              <Analytics
+                className="shareIcon"
+                htmlColor="red"
+              />
               <span className="shareIconText">投票</span>
             </div>
           </div>
-          <button className="shareButton">投稿</button>
+          <button className="sharePostButton">投稿</button>
         </div>
       </div>
     </div>
