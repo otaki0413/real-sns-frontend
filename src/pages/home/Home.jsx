cfb29dcfb4c198d77890a669fca9b205df048848
otaki0413 <takida0413@gmail.com>
2022-12-13 22:34:28 +0900

    💄 Homeコンポーネントの配置を調整

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,14 +2,17 @@ import { Rightbar } from "../../components/rightbar/Rightbar";
 import { Sidebar } from "../../components/sidebar/Sidebar";
 import { Timeline } from "../../components/timeline/Timeline";
 import { Topbar } from "../../components/topbar/Topbar";
+import "./Home.css";
 
 export const Home = () => {
   return (
     <div>
       <Topbar />
-      <Sidebar />
-      <Timeline />
-      <Rightbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Timeline />
+        <Rightbar />
+      </div>
     </div>
   );
 };
