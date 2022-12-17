94bca6d1699cab650b5270d4deed6af895de9bfc
otaki0413 <takida0413@gmail.com>
2022-12-18 02:20:25 +0900

    💄 Rightbarコンポーネントのスタイリング

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,5 +1,45 @@
 import "./rightbar.css";
+import profileImg from "../../assets/person/1.jpeg";
+import starImg from "../../assets/star.png";
+import eventImg from "../../assets/event.jpeg";
 
 export const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="eventContainer">
+          <img
+            src={starImg}
+            alt="スター画像"
+            className="starImg"
+          />
+          <span className="eventText">
+            <b>フォロワー限定</b>イベント開催中
+          </span>
+        </div>
+        <img
+          src={eventImg}
+          alt="イベント画像"
+          className="eventImg"
+        />
+        <h4 className="rightbarTitle">オンラインの友達</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={profileImg} alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnlineBadge"></span>
+            </div>
+            <span className="rightbarUserName">otaki</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img src={profileImg} alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnlineBadge"></span>
+            </div>
+            <span className="rightbarUserName">otaki</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
