f5de050771f944ffbd2e31a96b6bff0e21099fe5
otaki0413 <takida0413@gmail.com>
2022-12-24 19:21:57 +0900

    ✨ Loginコンポーネントの雛形を作成

---
+++ src/pages/login/ Login.jsx
@@ -0,0 +1,15 @@
+import "./Login.css";
+
+export const Login = () => {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">Real SNS</h3>
+          <span className="loginDesc">本格的なSNSを自分の手で！</span>
+        </div>
+        <div className="loginRight">fjaljfla</div>
+      </div>
+    </div>
+  );
+};
