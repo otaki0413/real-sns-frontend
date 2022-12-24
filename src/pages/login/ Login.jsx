a37a6c1c3f44d3e447e939a28bf9743791824207
otaki0413 <takida0413@gmail.com>
2022-12-25 01:10:47 +0900

    ✨ ログインコンポーネントの作成とスタイリング

--- src/pages/login/ Login.jsx
+++ src/pages/login/ Login.jsx
@@ -5,10 +5,27 @@ export const Login = () => {
     <div className="login">
       <div className="loginWrapper">
         <div className="loginLeft">
-          <h3 className="loginLogo">Real SNS</h3>
+          <h2 className="loginLogo">Real SNS</h2>
           <span className="loginDesc">本格的なSNSを自分の手で！</span>
         </div>
-        <div className="loginRight">fjaljfla</div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <p className="loginMsg">ログインはこちら</p>
+            <input
+              type="text"
+              className="loginInput"
+              placeholder="Eメール"
+            />
+            <input
+              type="text"
+              className="loginInput"
+              placeholder="パスワード"
+            />
+            <button className="loginButton">ログイン</button>
+            <span className="loginForgot">パスワードを忘れた方へ</span>
+            <button className="loginRegisterButton">アカウント作成</button>
+          </div>
+        </div>
       </div>
     </div>
   );
