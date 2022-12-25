ce51b73b72698455c62aa2a43a712993234763eb
otaki0413 <takida0413@gmail.com>
2022-12-25 18:00:25 +0900

    ✨ パスに応じてページを切り替えられるようにルーティング設定した

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-// import { Profile } from "src/pages/profile/Profile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home/Home";
-// import { Login } from "src/pages/login/ Login";
-import { Register } from "src/pages/regisiter/Register";
+import { Login } from "./pages/login/ Login";
+import { Register } from "./pages/regisiter/Register";
+import { Profile } from "./pages/profile/Profile";
 
 function App() {
-  return <Home />;
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/profile/:username" element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
